fad21f18e1db20f234ca33aa34dd4e682f546b94
Coffee-Dragon <alina.offic@gmail.com>
2020-10-12 21:16:16 +0200

    Started to rewrite code into OOP

---
+++ cell.js
@@ -0,0 +1,17 @@
+class Cell {
+    /** @param div {HTMLDivElement} HTML element that represents this cell on the UI */
+    constructor (div) {
+        this.div = div;
+        this.isAlive = false;
+    }
+
+    resurrect() {
+        this.isAlive = true;
+        this.div.classList.add('live');
+    }
+
+    die() {
+        this.isAlive = false;
+        this.div.classList.remove('live');
+    }
+}
--- script.js
+++ script.js
@@ -1,32 +1,28 @@
+
 const field = document.querySelector('.field');
 const fieldFrag = document.createDocumentFragment();
 const cells = [];
-let liveOrDead = [];
 const rowLength = 10;
 let isIntervalOn = false;
 
 for (let i=0; i<100; i++) {
-    const cell = document.createElement('div');
-    fieldFrag.appendChild(cell);
-    cells.push(cell);
+    const div = document.createElement('div');
+    fieldFrag.appendChild(div);
+    cells.push(new Cell(div));
 }
 
 field.appendChild(fieldFrag);
-
 const startBtn = document.querySelector('.btn-bar__btn--start');
 /**
  * Randomly attributes live class to div cells inside the playing field.
- * Randomly fills liveOrDead and cells arrays.
  */
 function seedLife() {
     for (let i=0; i < cells.length; i++) {
         const cell = cells[i];
         if (Math.random() > 0.5) {
-            cell.classList.add('live');
-            liveOrDead[i] = true;
+            cell.resurrect();
         } else {
-            cell.classList.remove('live');
-            liveOrDead[i] = false;
+            cell.die();
         }
     }
 }
@@ -35,6 +31,7 @@ let intervalID;
 
 const nextStep = () => {
     const nextAliveOrDead = [];
+    const aliveOrDead = cells.map(element => element.isAlive);
     for (let i = 0; i < cells.length; i++) {
         const isLeftEdge = (i % rowLength) === 0;
         const isTopEdge = i < rowLength;
@@ -42,51 +39,51 @@ const nextStep = () => {
         const isBottomEdge = i >= (cells.length - rowLength);
         let liveNeighbourCount = 0;
         // checking top-left neighbour cell
-        if (!isLeftEdge && !isTopEdge && liveOrDead[i - rowLength - 1]) {
+        if (!isLeftEdge && !isTopEdge && aliveOrDead[i - rowLength - 1]) {
             liveNeighbourCount++;
         }
         // checking top neighbour cell
-        if (!isTopEdge && liveOrDead[i - rowLength]) {
+        if (!isTopEdge && aliveOrDead[i - rowLength]) {
             liveNeighbourCount++;
         }
         // checking top-right neighbour cell
-        if (!isTopEdge && !isRightEdge && liveOrDead[i - rowLength + 1]) {
+        if (!isTopEdge && !isRightEdge && aliveOrDead[i - rowLength + 1]) {
             liveNeighbourCount++;
         }
         // checking left neighbour cell
-        if (!isLeftEdge && liveOrDead[i - 1]) {
+        if (!isLeftEdge && aliveOrDead[i - 1]) {
             liveNeighbourCount++;
         }
         // checking right neighbour cell
-        if (!isRightEdge && liveOrDead[i + 1]) {
+        if (!isRightEdge && aliveOrDead[i + 1]) {
             liveNeighbourCount++;
         }
         // checking bottom-left neighbour cell
-        if (!isLeftEdge && !isBottomEdge && liveOrDead[i + rowLength - 1]) {
+        if (!isLeftEdge && !isBottomEdge && aliveOrDead[i + rowLength - 1]) {
             liveNeighbourCount++;
         }
         // checking bottom neighbour cell
-        if (!isBottomEdge && liveOrDead[i + rowLength]) {
+        if (!isBottomEdge && aliveOrDead[i + rowLength]) {
             liveNeighbourCount++;
         }
         // checking bottom-right neighbour cell
-        if (!isBottomEdge && !isRightEdge && liveOrDead[i + rowLength +1]) {
+        if (!isBottomEdge && !isRightEdge && aliveOrDead[i + rowLength +1]) {
             liveNeighbourCount++;
         }
-        nextAliveOrDead[i] = liveOrDead[i] 
+        nextAliveOrDead[i] = aliveOrDead[i] 
             ? liveNeighbourCount >= 2 && liveNeighbourCount <= 3
             : liveNeighbourCount === 3;
         if (nextAliveOrDead[i]) {
-            cells[i].classList.add('live');
+            cells[i].resurrect();
         } else {
-            cells[i].classList.remove('live');
+            cells[i].die();
         }
     }
     let shouldStop = true;
     let hasLiveCells = false;
 
     for (let i = 0; i < nextAliveOrDead.length; i++) {
-        if (nextAliveOrDead[i] !== liveOrDead[i]) {
+        if (nextAliveOrDead[i] !== aliveOrDead[i]) {
             shouldStop = false;
             
         }
@@ -98,7 +95,6 @@ const nextStep = () => {
         stop();
     }
 
-    liveOrDead = nextAliveOrDead;
 }
 
 const start = () => {
